a1f6e2d767ae4b9c9154df4b25ce5c628f455001
Oleg Bas <olegbas1996@gmail.com>
2020-12-08 14:12:28 +0300

    intalling and ajusting of webpack.devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    open: true,
   },
 };
