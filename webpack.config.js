994d8f303a9501aaf0028adbf46eed74f9e8925c
Oleg Bas <olegbas1996@gmail.com>
2020-12-09 09:38:31 +0300

    installing and ajustment pug in project

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require("path");
+const fs = require("fs");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const PATHS = {
+  src: path.resolve(__dirname, "src"),
+  dist: path.resolve(__dirname, "dist"),
+  assets: "assets/",
+};
+
+const PAGES_DIR = `${PATHS.src}/pages`;
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith(".pug"));
 
 module.exports = {
   entry: "./src/index.js",
@@ -38,15 +50,22 @@ module.exports = {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         type: "asset/inline",
       },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./index.html"), // шаблон
-      filename: "index.html", // название выходного файла
-    }),
     new MiniCssExtractPlugin({
       filename: `[name].min.css`,
     }),
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page.replace(/\.pug/, ".html")}`,
+        })
+    ),
   ],
 };
