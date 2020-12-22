baa9d23f90df46480d831d668bb4dcc2e0edc4df
Oleg Bas <olegbas1996@gmail.com>
2020-12-22 11:02:42 +0300

    add element masked text field on page form-elements

---
+++ src/components/input/input.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import "jquery-mask-plugin";
+
+$('.formElement__input[name="user-masks"]').mask("dD.mM.THDY", {
+  translation: {
+    d: { pattern: /[0-3]/, recursive: true },
+    D: { pattern: /[0-9]/, recursive: true },
+    m: { pattern: /[0-1]/, recursive: true },
+    M: { pattern: /[0-9]/, recursive: true },
+    T: { pattern: /[1-2]/, recursive: true },
+    H: { pattern: /[9,0]/, recursive: true },
+    D: { pattern: /[0-9]/, recursive: true },
+    Y: { pattern: /[0-9]/, recursive: true },
+  },
+});
+
+$('.formElement__input[type="email"]').mask("email", {
+  translation: {
+    email: { pattern: /[\w@\-.+]/, recursive: true },
+  },
+});
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,3 +2,4 @@
 // создание свойства класса без конструктора
 import "material-design-icons/iconfont/material-icons.css";
 import "../../styles/main.scss";
+import "../../components/input/input";
