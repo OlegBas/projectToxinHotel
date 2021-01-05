3c8481fbc051d0b40a9f4f91668ad66fa1841ba7
Oleg Bas <olegbas1996@gmail.com>
2021-01-05 15:13:14 +0300

    adding component of footer

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,7 @@
 // index.js
 // создание свойства класса без конструктора
 import "material-design-icons/iconfont/material-icons.css";
+import "font-awesome/css/font-awesome.min.css";
 import "../../styles/main.scss";
 import "../../components/input/input";
 import "../../components/rangeSlider/rangeSlider";
