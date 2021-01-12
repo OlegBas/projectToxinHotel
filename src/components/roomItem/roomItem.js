4dc89269d46f01aa93aad800a63a704819f53636
Oleg Bas <olegbas1996@gmail.com>
2021-01-12 13:52:05 +0300

    creating layout on default for site pages

--- src/components/roomItem/roomItem.js
+++ src/components/roomItem/roomItem.js
@@ -2,15 +2,15 @@ import "slick-carousel";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-$(".room-card__gallery").slick({
+$(".roomItem__gallery").slick({
   dots: true,
   adaptiveHeight: true,
   nextArrow: getButton("next"),
   prevArrow: getButton("before"),
 });
 
-// type = before | next
+// // type = before | next
 function getButton(type) {
   const icon = `<i class="material-icons">navigate_${type}</i>`;
-  return `<button class="room-card__gallery-arrow room-card__gallery-arrow--${type}">${icon}</button>`;
+  return `<button class="roomItem__galleryArrow roomItem__galleryArrow_${type}">${icon}</button>`;
 }
